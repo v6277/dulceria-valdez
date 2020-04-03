98075d506444ba12ebfbaafa97c5f0fa06650b97
v6277 <vphysicist@gmail.com>
2020-04-03 15:11:42 -0700

    improved form html and added test css

--- send_form_email.php
+++ send_form_email.php
@@ -80,6 +80,7 @@ $headers = 'From: '.$email_from."\r\n".
 <meta name="Keywords" content="Dulceria, Dulceria Valdez">
 <meta charset="utf-8">
 <link rel="stylesheet" href="stylesheets/styles.css">
+<link rel="stylesheet" href="stylesheets/form-styles.css">
 <link rel="icon" type="image/png" href="/favicon-16x16.png" sizes="16x16">
 <link rel="icon" type="image/png" href="/favicon-32x32.png" sizes="32x32">
 </head>
@@ -97,8 +98,10 @@ $headers = 'From: '.$email_from."\r\n".
              class="logo">
     </div>
 
-¡Gracias por contactárnos! 
-Estaremos en contácto con usted pronto.
+    <div class="form-message"> 
+      <h1>¡Gracias por contactárnos!</h1>
+      <h2>Estaremos en contácto con usted pronto.</h2>
+    </div>
 
 </body>
 
